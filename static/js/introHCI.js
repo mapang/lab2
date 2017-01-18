704c4e8c8fb13573e1e321f8b83e579a7ec22242
Michelle Pang <mapang@eng.ucsd.edu>
2017-01-18 12:12:14 -0800

    added CSS and Bootstrap

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -12,7 +12,11 @@ function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
-
+function projectClick(e){
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+}
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
